843df0de08817d5210f0df15d4094f2e86b8a41c
Danylo <danavinnickij@gmail.com>
2025-12-26 13:17:56 +0200

    training

--- src/data/trainings.js
+++ src/data/trainings.js
@@ -1,3 +1,9 @@
+import yogaImg from './assets/img/yoga.jpeg';
+import pilatesImg from './assets/img/pilates.jpeg';
+import hiitImg from './assets/img/hiit.jpeg';
+import boxingImg from './assets/img/boxing.jpeg';
+
+
 export default {
   trainings: [
     {
@@ -7,7 +13,7 @@ export default {
       description: 'Relaxing and stretching exercises.',
       timeSlots: ['08:00 - 09:00', '10:00 - 11:00'],
       duration: 60,
-      image: 'https://images.pexels.com/photos/1051838/pexels-photo-1051838.jpeg',
+      image: yogaImg,
       favorites: false
     },
     {
@@ -17,7 +23,7 @@ export default {
       description: 'Core strength and flexibility training.',
       timeSlots: ['09:30 - 10:30', '11:30 - 12:30'],
       duration: 60,
-      image: 'https://images.pexels.com/photos/3757955/pexels-photo-3757955.jpeg',
+      image: pilatesImg,
       favorites: true
     },
     {
@@ -27,7 +33,7 @@ export default {
       description: 'High intensity interval training.',
       timeSlots: ['11:00 - 11:45', '12:00 - 12:45'],
       duration: 45,
-      image: 'https://images.pexels.com/photos/6390240/pexels-photo-6390240.jpeg',
+      image: hiitImg,
       favorites: true
     },
     {
@@ -37,7 +43,7 @@ export default {
       description: 'Intense cardio and strength workout.',
       timeSlots: ['12:00 - 12:55', '13:00 - 13:55'],
       duration: 55,
-      image: 'https://images.pexels.com/photos/7187935/pexels-photo-7187935.jpeg',
+      image: boxingImg,
       favorites: false
     }
   ]
